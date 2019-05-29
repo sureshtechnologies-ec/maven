92da8f549766d515ef18404b71564fbc33ae5a9d
suresh <sureshdevops1010@gmail.comgit config --global user.emailgit config --global user.email sureshdevops1010@gmail.com>
2019-05-30 00:25:27 +0530

    maven commit

---
+++ src/main/java/samplegroup/App.java
@@ -0,0 +1,13 @@
+package samplegroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
